032fa260a7e6eeb7adf4dd0197267b2122f16755
abiha456 <abiha.amir456@gmail.com>
2020-08-11 10:06:02 -0500

    psd

---
+++ wp-content/themes/Divi-child/demo.php
@@ -0,0 +1,220 @@
+<?php 
+
+/* Template Name: Demo */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PSD</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600;700;900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(). '/psd.css'?>">
+    </head>
+    <body>
+        <header class="hundred">
+            <div class="head-bgcolor">
+                <div class="container">
+                    <div class="row">
+                        <div class="left-area">
+                            <i class="fa fa-envelope"></i>
+                            <p class="p1">info@reen.com</p>
+                            <i class="fa fa-mobile"></i>
+                            <p class="p1">+00 (123) 456 78 90</p>
+                        </div>
+                        <div class="right-area">
+                            <i class="fa fa-facebook-f"></i>
+                            <i class="fa fa-google-plus"></i>
+                            <i class="fa fa-twitter"></i>
+                            <i class="fa fa-pinterest-p"></i>
+                            <i class="fa fa-behance"></i>
+                            <i class="fa fa-dribbble"></i>
+                        </div>
+                    </div>      
+                </div>            
+            </div>  
+
+            <div class="container">
+                <div class="row">
+                    <div class="logo">
+                        <img src="<?php echo get_stylesheet_directory_uri(). '/images/logo_03.png'?>" alt="logo">
+                    </div>
+                    <div class="main-menu">
+                        <ul>
+                            <li class="list-items">Home</li>
+                            <li class="list-items">Portfolio</li>
+                            <li class="list-items">Blog</li>
+                            <li class="list-items">Pages</li>
+                            <li class="list-items">Features</li>
+                            <li class="list-items">Mega Menu</li>
+                            <li class="list-items">Contact</li>
+                        </ul>
+                    </div>
+                    <div class="srch-icon">
+                        <i class="fa fa-search"></i>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <main>
+            <section class="hundred banner">
+                <div class="container">
+                    <h1 class="h1">Made for Designers</h1>
+                    <div class="para">
+                        <p class="p2">Create your online portfolio in minutes with the professionally and lovingly designed REEN website template. Customize your site with versatile and easy to use features.</p>
+                    </div>
+                    <div class="btn">
+                        <a href="#">Get Started Now</a>
+                    </div>
+                </div>
+            </section>
+
+            <section class="hundred">
+                <div class="container">
+                    <h2 class="h2">Beautiful. Clean. Responsive.</h2>
+                    <div class="para">
+                        <p class="p3">REEN is a high-quality solution for those who want a beautiful website in no time. It's fully responsive and will adapt itself to any mobile device. iPad, iPhone, Android, it doesn't matter. Your content will always looks its absolute best.</p>
+                    </div>
+
+                    <div class="row">
+                        <div class="half-col1">
+                            <div class="icon">
+                                <i class="fa fa-heart"></i>
+                            </div>
+                            <div class="content">
+                                <h3 class="h3">Passion</h3>
+                                <p class="p4">Magnis modipsae que lib voloratati andigen daepeditem quiate re porem aut labor. Laceaque quiae sitiorem rest non restibusaes maio es dem tumquam core posae volor remped modis volor.</p>
+                            </div>
+                        </div>
+                        <div class="half-col1">
+                            <div class="icon bulb">
+                            </div>
+                            <div class="content">
+                                <h3 class="h3">Creativity</h3>
+                                <p class="p4">Magnis modipsae que lib voloratati andigen daepeditem quiate re porem aut labor. Laceaque quiae sitiorem rest non restibusaes maio es dem tumquam core posae volor remped modis volor.</p>
+                            </div>
+                        </div>
+                        <div class="half-col1">
+                            <div class="icon">
+                                <i class="fa fa-star"></i>
+                            </div>
+                            <div class="content">
+                                <h3 class="h3">Quality</h3>
+                                <p class="p4">Magnis modipsae que lib voloratati andigen daepeditem quiate re porem aut labor. Laceaque quiae sitiorem rest non restibusaes maio es dem tumquam core posae volor remped modis volor.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="hundred bg-color">
+                <div class="container">
+                    <h2 class="h2">Check out our latest works</h2>
+                    <div class="para">
+                        <p class="p3">Magnis modipsae que voloratati andigen daepeditem quiate re porem aut labor.</p>
+                    </div>
+
+                    <div class="row">
+                        <div class="half-col2">
+                            <div class="color">
+                                <div class="img">
+                                    <img src="<?php echo get_stylesheet_directory_uri(). '/images/img1_03.png'?>" alt="img">
+                                </div>
+                                <div class="text">
+                                    <p class="p5">Grand Motel gets an identity brushup</p>
+                                    <p class="p6">identity</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="half-col2">
+                            <div class="img">
+                                <img src="<?php echo get_stylesheet_directory_uri(). '/images/img2_03.png'?>" alt="img">
+                            </div>
+                            <div class="text">
+                                <p class="p5">Fresh branding for a creative startup</p>
+                                <p class="p6">identity</p>
+                            </div>
+                        </div>
+                    </div>
+
+                <div class="row">
+                    <div class="items">
+                        <img src="<?php echo get_stylesheet_directory_uri(). '/images/item1_03.png'?>" alt="item">
+                    </div>
+                    <div class="items">
+                        <img src="<?php echo get_stylesheet_directory_uri(). '/images/item2_03.png'?>" alt="item">
+                    </div>
+                    <div class="items">
+                        <img src="<?php echo get_stylesheet_directory_uri(). '/images/item3_03.png'?>" alt="item">
+                    </div>
+                    <div class="items">
+                        <img src="<?php echo get_stylesheet_directory_uri(). '/images/item4_03.png'?>" alt="item">
+                    </div>
+                </div>
+                </div>
+            </section>
+
+            <section class="hundred work-sec">
+                <div class="container">
+                    <h2 class="h2">Want to work with us?</h2>
+                    <div class="para">
+                        <p class="p3">Magnis modipsae que voloratati andigen daepeditem quiate re porem aut labor. Laceaque quiae sitiorem rest non restibusaes maio es dem tumquam.</p>
+                    </div>
+                    <div class="btn">
+                        <a href="#">get in touch</a>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <footer class="hundred">
+            <div class="footer-bgcolor">
+                <div class="container">
+                    <div class="row">
+                        <div class="half-col3">
+                            <h3 class="h4">Who we are</h3>
+                            <img src="<?php echo get_stylesheet_directory_uri(). '/images/logo1_03.png'?>" alt="logo">
+                            <p class="p7">Magnis modipsae voloratati andigen daepeditem quiate re porem que aut labor. Laceaque eictemperum quiae sitiorem rest non restibusaes maio es dem tumquam.</p>
+                            <p class="p8">More about us</p>
+                        </div>
+                        <div class="half-col3">
+                            <h3 class="h4">Latest Works</h3>
+                            <img src="<?php echo get_stylesheet_directory_uri(). '/images/thumb_03.png'?>" alt="logo">
+                        </div>
+                        <div class="half-col3">
+                            <h3 class="h4">get in touch</h3>
+                            <p class="p7">Doloreiur quia commolu ptatemp dolupta oreprerum tibusam eumenis et consent accullignis dentibea autem inisita.</p>
+                            <i class="fa fa-map-marker"></i><p class="p9">84 Street, City, State 24813</p>
+                            <i class="fa fa-mobile"></i><p class="p9">+00 (123) 456 78 90</p>
+                            <i class="fa fa-envelope"></i><p class="p9">info@reen.com</p>
+                        </div>
+                        <div class="half-col3">
+                            <h3 class="h4">Free updates</h3>
+                            <p class="p7">Conecus iure posae volor remped modis aut lor volor accabora incim resto explabo.</p>
+                            <div class="footer-input">
+                                <input type="text" placeholder="Enter your email address" class="input1">
+                                <input type="submit" value="Subscribe" class="input2">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer-color">
+                <i class="fa fa-copyright"></i><p class="p10">2014 REEN. All rights reserved.</p>
+                <div class="footer-nav">
+                    <p class="p11">home</p>
+                    <p class="p11">portfolio</p>
+                    <p class="p11">blog</p>
+                    <p class="p11">about</p>
+                    <p class="p11">services</p>
+                    <p class="p11">contact</p>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
